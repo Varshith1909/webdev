97203caebc40b71004050e3d3b378f13db000e77
Varshith1909 <vc8@pdx.edu>
2023-03-06 17:18:33 -0800

    HW - 5
    Final Code

--- hw5/02-beverages.js
+++ hw5/02-beverages.js
@@ -1,10 +1,64 @@
 const url =
-  'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic';
+  "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
 
-let app = document.querySelector('#results');
+let app = document.querySelector("#results");
+
+const addElements = (drink) => {
+  let element = document.createElement("div");
+  let image = document.createElement("img");
+  let bevarge = document.createElement("div");
+
+  image.src = drink.strDrinkThumb;
+  image.style.width = "150px";
+  image.style.height = "150px";
+  image.alt = "Image of " + drink.strDrink;
+  bevarge.textContent = drink.strDrink;
+
+  element.style.margin = "10px";
+  element.style.paddingBottom = "0px";
+  element.style.paddingTop = "10px";
+  element.style.marginLeft = "10px";
+  element.style.marginRight = "10px";
+  element.className = "figure";
+  element.style.display = "flex";
+  element.style.flexDirection = "column";
+  element.style.alignItems = "center";
+  element.style.width = "180px";
+
+  bevarge.style.paddingBottom = "10px";
+  bevarge.style.paddingTop = "30px";
+  bevarge.style.marginLeft = "25px";
+  bevarge.style.marginRight = "25px";
+  bevarge.style.fontWeight = "bold";
+  bevarge.style.fontSize = "20px";
+
+  element.appendChild(image);
+  element.appendChild(bevarge);
+
+  app.append(element);
+};
 
 const fetchData = (url) => {
   // Add your code here
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      data.drinks.forEach((drink) => {
+        addElements(drink);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+
+      let element = document.createElement("div");
+      element.textContent = "An error occured. Please try again.";
+      app.append(element);
+    })
+    .finally(() => {
+      let loader = document.querySelector("#loading");
+      app.removeChild(loader);
+    });
 };
 
 fetchData(url);
--- hw5/03-beverages.js
+++ hw5/03-beverages.js
@@ -1,6 +1,69 @@
 $(document).ready(function () {
   const url =
-    'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic';
+    "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
 
   // Add your code here
+
+  const addElements = (drink) => {
+    $("#results").append(
+      $("<div>")
+        .addClass("figure")
+        .css({
+          margin: "10px",
+          paddingBottom: "0px",
+          paddingTop: "10px",
+          marginRight: "10px",
+          marginLeft: "10px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "180px",
+        })
+        .append(
+          $("<img />")
+            .attr("src", drink.strDrinkThumb)
+            .attr("alt", "Image of" + drink.strDrink)
+            .css({
+              width: "150px",
+              height: "150px",
+            })
+        )
+        .append(
+          $("<div>").text(drink.strDrink).css({
+            paddingBottom: "10px",
+            paddingTop: "30px",
+            marginLeft: "25px",
+            marginRight: "25px",
+            fontWeight: "bold",
+            fontSize: "20px",
+          })
+        )
+    );
+  };
+
+  const fetchData = (url) => {
+    $.ajax({
+      type: "GET",
+      url: url,
+      dataType: "json",
+      success: (data) => {
+        data.drinks.forEach((drink) => {
+          addElements(drink);
+        });
+      },
+
+      error: (error) => {
+        console.error(error);
+        $("#results").append(
+          $("<div>").text("An error occured. Please try again")
+        );
+      },
+
+      complete: () => {
+        $("#loading").remove();
+      },
+    });
+  };
+
+  fetchData(url);
 });
